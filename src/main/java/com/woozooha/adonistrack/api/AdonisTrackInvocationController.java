fe09578cfa77b7593a6823ead358bd7d70e69eb6
woozoo73 <woozoo73@gmail.com>
2021-11-19 16:32:51 +0900

    Invocations UI.

--- src/main/java/com/woozooha/adonistrack/api/AdonisTrackInvocationController.java
+++ src/main/java/com/woozooha/adonistrack/api/AdonisTrackInvocationController.java
@@ -4,20 +4,33 @@ import com.woozooha.adonistrack.aspect.ProfileAspect;
 import com.woozooha.adonistrack.domain.Invocation;
 import com.woozooha.adonistrack.writer.History;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/adonis-track/invocations")
 public class AdonisTrackInvocationController {
 
-    @GetMapping("/adonis-track/invocations")
-    public List<Invocation> histories() {
+    @GetMapping
+    public List<Invocation.InvocationSummary> getInvocations() {
         History history = ProfileAspect.getConfig().getHistory();
 
-        List<Invocation> invocationList = history.getInvocationList();
+        List<Invocation> invocations = history.getInvocationList();
 
-        return invocationList;
+        return invocations.stream().map(Invocation.InvocationSummary::new).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public Invocation getInvocation(@PathVariable String id) {
+        History history = ProfileAspect.getConfig().getHistory();
+
+        List<Invocation> invocations = history.getInvocationList();
+
+        return invocations.stream().filter(i -> id.equals(i.getId())).findFirst().orElse(null);
     }
 
 }
